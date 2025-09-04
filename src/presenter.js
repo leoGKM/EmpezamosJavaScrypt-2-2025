75b015ba8a072da7f7f3188fc0acf26c3a22f962
Leonardo Koller <leonardo.koller@ucb.edu.bo>
2025-09-04 07:38:56 -0400

    se añadio la funcion de multiplicar los 2 numeros

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,29 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+const sumarButton = document.querySelector("#sumar-button");
+const multiplicarButton = document.querySelector("#multiplicar-button");
+
+sumarButton.addEventListener("click", (event) => {
+  event.preventDefault(); 
 
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>Resultado de la suma: " + sumar(firstNumber, secondNumber) + "</p>";
 });
+
+multiplicarButton.addEventListener("click", (event) => {
+  event.preventDefault();  
+
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+
+  div.innerHTML = "<p>Resultado de la multiplicación: " + multiplicar(firstNumber, secondNumber) + "</p>";
+});
+
